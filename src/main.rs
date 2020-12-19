bc448d61510143d44e9ee1535e007d32c262e527
wsandst <williamsandstrom99@hotmail.com>
2020-12-19 22:31:02 +0100

    Add methods to render a buffer to the screen

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ mod renderer;
 fn main() {
     let mut renderer = renderer::Renderer::new();
 
+    let mut buffer : [u8; 160*144*3] = [0; 160*144*3];
+
+    for i in 0..(buffer.len()/3) {
+        buffer[i*3+0] = 0;
+        buffer[i*3+1] = 255;
+        buffer[i*3+2] = 255;
+    }
+
+    println!("S {}", buffer.len());
+    renderer.set_screen_buffer(&mut buffer);
+
     // Main game loop
     loop 
     {  
--- src/renderer.rs
+++ src/renderer.rs
@@ -10,9 +10,13 @@ use std::time::Duration;
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 720;
 
+const GB_SCREEN_WIDTH: usize = 160;
+const GB_SCREEN_HEIGHT: usize = 144;
+
 // Struct which contains the render state and various render methods
 pub struct Renderer
 {
+    screen_texture : sdl2::render::Texture,
     canvas : sdl2::render::Canvas<sdl2::video::Window>,
     event_pump : sdl2::EventPump
 }
@@ -33,18 +37,30 @@ impl Renderer
         canvas.set_draw_color(Color::RGB(0, 255, 255));
         canvas.clear();
         canvas.present();
+
+        let texture_creator =  canvas.texture_creator();
+        let texture = texture_creator.create_texture_streaming(sdl2::pixels::PixelFormatEnum::RGB24, GB_SCREEN_WIDTH as u32, GB_SCREEN_HEIGHT as u32).unwrap();
     
         let event_pump = sdl_context.event_pump().unwrap();
     
-        return Renderer {canvas: canvas, event_pump: event_pump};
+        return Renderer {screen_texture: texture, canvas: canvas, event_pump: event_pump};
     }
 
     pub fn render(&mut self)
     {
-        self.canvas.set_draw_color(Color::RGB(255, 64, 255));
         self.canvas.clear();
+        self.canvas.copy(&self.screen_texture, None, Some(sdl2::rect::Rect::new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))).unwrap();
         self.canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    // Set the screen texture to a buffer array of size GB_HEIGHT*GB_WIDTH*3
+    pub fn set_screen_buffer(&mut self, buffer : &mut [u8])
+    {
+        self.screen_texture.with_lock(None, |tbuffer: &mut [u8], _| {
+            println!("T {}", tbuffer.len());
+            tbuffer.copy_from_slice(buffer);
+        }).unwrap();
     }
 
     pub fn input(&mut self) -> bool
