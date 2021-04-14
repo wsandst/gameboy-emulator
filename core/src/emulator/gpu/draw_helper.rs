d685dce27bbe81585f08ddeae2cacfaa192313c9
wsandst <williamsandstrom99@hotmail.com>
2021-04-14 02:37:55 +0200

    Add documentation to draw_helper

--- core/src/emulator/gpu/draw_helper.rs
+++ core/src/emulator/gpu/draw_helper.rs
@@ -1,3 +1,6 @@
+/// Contains acceleration structures for Drawing, based on
+/// the tile memory in the GPU/PPU VRAM. These cache the gameboy represenations
+/// in a better format for modern rendering.
 use super::super::gpu;
 
 const SCREEN_WIDTH: usize = 160;
@@ -15,6 +18,7 @@ const COLOR_LIGHTGRAY: Color = Color {r:170,g:170,b:170};
 const COLOR_DARKGRAY: Color = Color {r:85,g:85,b:85};
 const COLOR_BLACK: Color = Color {r:0,g:0,b:0};
 
+/// Represents a 8x8 tile of Color
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Tile {
     pixels: [u8; 8*8*3],
@@ -31,15 +35,17 @@ impl Tile {
         self.pixels[y*8*3+x*3+2] = color.b;
     }
 
+    /// Generate the tile from the Tile representation in GPU VRAM
+    /// The lower and upper bits for the color are in separate bytes,
+    /// which makes the parsing somewhat convoluted
     fn generate(&mut self, id: usize, addr: usize, palette : &Palette, gpu_vram : &[u8; 8192]) {
         let offset_addr = (addr - 0x8000) + id*16;
         let data : &[u8] = &gpu_vram[offset_addr..offset_addr+16];
         let mut y = 0;
         let mut a : u8;
         let mut b : u8;
-        // This can be massively improved
-        // by pre-combining the bytes using an odd/even bitmask and shifting,
-        // we can half the ops.
+        // This can be massively improved by pre-combining the bytes 
+        // using an odd/even bitmask and shifting, which halfs the ops.
         // Furthermore, this can be done for all colors at once, as they bit in a 64 bit integer
         // Alternatively, a lookup table can be used (8 bits is just 256 values)
         // or pext.
@@ -56,6 +62,7 @@ impl Tile {
     }
 }
 
+/// Represents the 32x32 Tile Atlas, based on the Tilemap and Tiledata in the GPU/PPU
 pub struct TileAtlas {
     pub atlas: Vec<u8>,
 }
@@ -65,6 +72,7 @@ impl TileAtlas {
         TileAtlas { atlas : vec![0; 32*32*8*8*3] }
     }
 
+    /// Generate the atlas based on the tiles data and the tilemap in vram 
     pub fn generate(&mut self, addr: usize, tiles : &[Tile; 32*32], gpu_vram : &[u8; 8192]) {
         for y in 0..32 {
             for x in 0..32 {
@@ -79,6 +87,7 @@ impl TileAtlas {
         //self.blit_tile_to_map(x*8, y*8, y*32+x, tiles); // Display the tilemap
     }
 
+    /// Update all occurences of a tile, based on id, in the atlas.
     pub fn update_tiles_for_id(&mut self, addr : usize, id: usize, tiles : &[Tile; 32*32],  gpu_vram : &[u8; 8192]) {
         let id_u8 = id as u8;
         for y in 0..32 {
@@ -100,12 +109,18 @@ impl TileAtlas {
     }
 }
 
+/// Main helper class
 pub struct DrawHelper {
+    // We only keep one active atlas in memory, though there are
+    // technically 4 different atlases (tilemap1,tilemap2 | tiledata1, tiledata2),
+    // but only one is used at a time. We modify the atlas to match
+    // whatever combination is set.
     pub atlas : TileAtlas,
     pub tiles: [Tile; 32*32],
     pub background_palette : Palette,
     pub sprite_palette_1: Palette,
     pub sprite_palette_2: Palette,
+    // Which tile data and tile map should we use? From lcd_control
     pub using_tiledata_1: bool, // Reversed from the actual bit for easier to read code
     pub using_tilemap_1: bool,
 }
@@ -117,6 +132,7 @@ impl DrawHelper {
             sprite_palette_2 : Palette::new(), using_tiledata_1 : false, using_tilemap_1: true, }
     }
 
+    /// Generate the atlas from the active tilemap in VRAM
     pub fn generate_atlas(&mut self, gpu_vram: &[u8; 8192]) {
         if self.using_tilemap_1 {
             self.atlas.generate(0x9800, &self.tiles, gpu_vram);
@@ -126,6 +142,7 @@ impl DrawHelper {
         }
     }
 
+    /// Generate the tiles from the active tiledata in VRAM
     pub fn generate_tiles(&mut self, gpu_vram: &[u8; 8192]) {
         if self.using_tiledata_1 {
             for id in 0..256 {
@@ -139,6 +156,7 @@ impl DrawHelper {
         }
     }
 
+    /// Update the drawing acceleration structures based on a VRAM write
     pub fn update_by_vram_address(&mut self, address : usize, gpu_vram: &[u8; 8192]) {
         match address {
             0x8000 ..= 0x87FF => { self.update_tiledata_by_address(address, true, gpu_vram)} // Tile set #1: tiles 0-127
@@ -152,6 +170,8 @@ impl DrawHelper {
         }
     }
 
+    /// Update the tilemap based on a VRAM write. 0x9800-0x9FFF
+    /// As we only store one atlas
     pub fn update_tilemap_by_address(&mut self, address : usize, is_tilemap_1: bool, gpu_vram: &[u8; 8192]) {
         if self.using_tilemap_1 && is_tilemap_1 {
             let x = (address - 0x9800) % 32;
@@ -165,6 +185,7 @@ impl DrawHelper {
         }
     }
 
+    /// Update the tilemap based on a VRAM write. 0x9800-0x9FFF
     pub fn update_tiledata_by_address(&mut self, address : usize, is_tiledata_1: bool, gpu_vram: &[u8; 8192])
     {
         if self.using_tiledata_1 && is_tiledata_1 {
@@ -186,7 +207,6 @@ impl DrawHelper {
             else {
                 self.atlas.update_tiles_for_id(0x9C00, id, &self.tiles, gpu_vram);
             }
-            //self.generate_atlas(gpu_vram);
         }
     }
 
@@ -205,6 +225,8 @@ impl DrawHelper {
         return lcd_control & 0b0001_0000 == 0b0001_0000;
     }
 
+    /// Reversed from the actual bit here, as when the bit 4 is 0 means 8800-97FF 
+    /// and 1 means 8000-8FFF, which is a confusing scheme.
     pub fn get_background_tile_data_select(&self, lcd_control : u8) -> bool {
         return !(lcd_control & 0b0000_1000 == 0b0000_1000);
     }
@@ -214,6 +236,7 @@ pub struct Palette {
     palette: [Color; 4]
 }
 
+/// Represents a 4 color palette
 impl Palette {
     fn new() -> Palette {
         return Palette { palette: [COLOR_WHITE, COLOR_LIGHTGRAY, COLOR_DARKGRAY, COLOR_BLACK]}
