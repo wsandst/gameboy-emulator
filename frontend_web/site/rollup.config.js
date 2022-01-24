3eb08eebb258fdfbaeceb3f3955f04e0f87ef1df
wsandst <williamsandstrom99@hotmail.com>
2022-01-24 05:18:27 +0100

    Fix rollup-plugin-css-only dependency issue for web frontend

--- frontend_web/site/rollup.config.js
+++ frontend_web/site/rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import rust from "@wasm-tool/rollup-plugin-rust";
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
